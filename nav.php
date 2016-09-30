e1ab9e9a42be104673a2e3244a65bcf93835af79
Larry <tradesouthwest@outlook.com>
2016-09-29 21:18:41 -0700

    removed some
    removed monthly link added clone link

--- nav.php
+++ nav.php
@@ -2,9 +2,9 @@
     <ul class="list-inline">
     <li><a href="index.php" title="">home</a></li>
     <li><a href="tables.php" title="">tables</a></li>
-    <li><a href="tables.php" title="">monthly</a></li>
-    <li><a href="" title="">yearly</a></li>
     <li><a href="" title="">other</a></li>
+    <li><a href="" title="">yearly</a></li>
+    <li><a href="backup.php" title="">clone</a></li>
     <li><a href="inc/logout.php" title="">logout</a></li>
     </ul>
 </div>
